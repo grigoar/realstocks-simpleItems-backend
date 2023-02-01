b849ca17ad7911b7011cd484e4ea213d81200b28
grigoar <grigore.nath.j@gmail.com>
2023-02-01 08:57:55 +0200

    simple ci/cd lambda deploy

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+
+export const handler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
